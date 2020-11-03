cc20d007ce1e2490192b01833609e41fae4feed3
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2020-11-03 17:29:09 +0100

    Add UseCollection

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,5 +1,9 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
@@ -18,19 +22,28 @@ public final class UseCollection {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+    	final List <Integer> alist = new ArrayList<>();
+    	for( int i=1000 ; i<2000 ; i++ ) {
+    		alist.add(i);
+    	}
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+    	final List<Integer> llist = new LinkedList<>(alist);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
-        /*
+    	   
+    	/*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+    	for ( final int j : alist) {
+    		System.out.println(" " + alist);
+    	}
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -38,12 +51,14 @@ public final class UseCollection {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+    	
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+    	
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
